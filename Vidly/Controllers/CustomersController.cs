1b1e4c3d804efaa78cbd557c605b8ae6dc15a555
Ognjen Prica <priconije@gmail.com>
2018-01-20 00:22:51 +0100

    Display Discount Rate for each Customer on Customers page

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,37 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using WebApplication1.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int customerId)
         {
-            var customer = GetCustomers().Where(c => c.Id == customerId).FirstOrDefault();
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == customerId);
             return View(customer);
-        }
-
-        private List<Customer> GetCustomers()
-        {
-            var allCustomers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Ognjen Prica" },
-                new Customer { Id = 2, Name = "Sasa Prica" }
-
-            };
-
-            return allCustomers;
         }
     }
 }
