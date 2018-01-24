71dc295bba0c3466304e161145895a56b3e64afa
Ognjen Prica <priconije@gmail.com>
2018-01-24 23:12:48 +0100

    Adding Name to MembershipType(2/2)

--- Vidly/Migrations/201801241113141_PopulateMembershipTypeName.cs
+++ Vidly/Migrations/201801241113141_PopulateMembershipTypeName.cs
@@ -7,12 +7,13 @@ namespace Vidly.Migrations
     {
         public override void Up()
         {
-            Sql("UPDATE MembershipTypes SET Name = 'Neko ime' WHERE Id = 1");
-            Sql("UPDATE MembershipTypes (Id, SignupFee, DurationInMonths, DiscountRate) VALUES (2, 30, 1, 10)");
-            Sql("UPDATE MembershipTypes (Id, SignupFee, DurationInMonths, DiscountRate) VALUES (3, 90, 3, 15)");
-            Sql("UPDATE MembershipTypes (Id, SignupFee, DurationInMonths, DiscountRate) VALUES (4, 300, 12, 20)");
+            Sql("UPDATE MembershipTypes SET Name = 'Pay As You Go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET Name = 'Quarterly' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET Name = 'Annualy' WHERE Id = 4");
+
         }
-        
+
         public override void Down()
         {
         }
